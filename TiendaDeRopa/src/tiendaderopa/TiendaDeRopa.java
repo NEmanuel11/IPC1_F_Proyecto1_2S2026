4616f3d42bcf6181a33e7ecf655b9d292f3f9cad
NehemiasIxcoy1 <3019408000101@ingenieria.usac.edu.gt>
2026-03-08 21:46:29 -0600

    Primer Commit: Implementacion de Ventanas.

---
+++ TiendaDeRopa/src/tiendaderopa/TiendaDeRopa.java
@@ -0,0 +1,22 @@
+
+package tiendaderopa;
+
+import tiendaderopa.views.VistaPrincipal;
+
+/**
+ *
+ * @author Emanuel López
+ */
+public class TiendaDeRopa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VistaPrincipal principal = new VistaPrincipal();
+        principal.setVisible(true);
+        principal.setTitle("Menu Principal");
+
+    }
+    
+}
